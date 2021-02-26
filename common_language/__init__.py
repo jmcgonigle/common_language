6c8fa478e8ea841e677d08f653c4746ac35dd2b0
John McGonigle <john.mcgonigle@horizondiscovery.com>
2021-02-26 13:00:17 +0000

    Updated the release to support multiple files.

--- common_language/__init__.py
+++ common_language/__init__.py
@@ -3,20 +3,21 @@ from common_language.convert_spellings import convert_spelling
 
 
 @click.command()
-@click.argument("input-file", required=True, type=click.Path(exists=True))
-@click.argument("output-file", required=False, default=None, type=str)
+@click.argument('input-files', nargs=-1, type=click.Path(exists=True), required=True)
+@click.option("--output-file", required=False, default=None, type=str)
 @click.option('--britishise', required=False, default=False, is_flag=True)
-def main(input_file, output_file, britishise):
+def main(input_files, output_file, britishise):
     if britishise:
         british_to_american = False
     else:
         british_to_american = True
 
-    convert_spelling(
-        filepath=input_file,
-        output_filepath=output_file,
-        british_to_american=british_to_american
-    )
+    for input_file in input_files:
+        convert_spelling(
+            filepath=input_file,
+            output_filepath=output_file,
+            british_to_american=british_to_american
+        )
 
 
 if __name__ == '__main__':
